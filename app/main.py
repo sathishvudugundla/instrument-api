99d4d2dffb37226339fac24e54765efdd2416025
sathishvudugundla <satheesh4d0@gmail.com>
2025-06-15 09:28:23 +0530

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,20 +1,3 @@
-# from fastapi import FastAPI, UploadFile, File
-# from keras.models import load_model
-
-# app = FastAPI()
-# model = None
-
-# @app.get("/")
-# async def read_root():
-#     return {"status": "running"}
-
-# @app.post("/predict/")
-# async def predict(file: UploadFile = File(...)):
-#     global model
-#     if model is None:
-#         model = load_model("app/instrument_model.h5")
-#     return {"result": "prediction logic here"}
-
 import numpy as np
 import librosa
 from fastapi import FastAPI, UploadFile, File
